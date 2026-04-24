a14a9a8cf801085fc845d9581dfb92090b899bc1
nirobmondal <nirobmondal202@gmail.com>
2026-04-24 16:59:18 +0600

    basic routing done

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hello niramoy</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
